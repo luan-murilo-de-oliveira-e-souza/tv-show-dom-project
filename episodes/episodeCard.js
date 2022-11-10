0d88a4a4eaf5796320d28e4b22a89b3b62a16080
luan-murilo-de-oliveira-e-souza <luan.oliveiraesouza@gmail.com>
2022-11-10 20:50:30 +0000

    load all series

--- episodes/episodeCard.js
+++ episodes/episodeCard.js
@@ -1,4 +1,5 @@
 export function episodeCard(episodes) {
+  console.log(episodes);
   const gallerySeries = document.getElementById("gallerySeries");
   gallerySeries.innerHTML = "";
 
@@ -8,37 +9,48 @@ export function episodeCard(episodes) {
 
     //Episode Name
     const nameEpisode = document.createElement("h3");
-    nameEpisode.innerText = `${episode.name} - S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
-    episodeCardItem.appendChild(nameEpisode);
+
+    if (episode.season && episode.number && episode.name) {
+      nameEpisode.innerText = `${episode.name} - S${episode.season
+        .toString()
+        .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+      episodeCardItem.appendChild(nameEpisode);
+    }
 
     //Episode Img
-    const episodeImg = document.createElement("img");
-    episodeImg.src = episode.image.medium;
-    episodeImg.alt = `${episode.name} - S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
-    episodeCardItem.appendChild(episodeImg);
+    if (episode.image && episode.image.medium) {
+      const episodeImg = document.createElement("img");
+      episodeImg.src = episode.image.medium;
+
+      episodeImg.alt = `${episode.name} - S${episode.season
+        .toString()
+        .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+
+      episodeCardItem.appendChild(episodeImg);
+    }
 
     //Episode summary
     const episodeSummary = document.createElement("div");
     episodeSummary.className = "summary";
 
-    if (episode.summary.length > 150) {
-      episodeSummary.innerHTML = `${episode.summary.substring(0, 150)}...`;
-    } else {
-      episodeSummary.innerHTML = `${episode.summary.substring(0, 150)}`;
+    if (episode.summary) {
+      if (episode.summary.length > 150) {
+        episodeSummary.innerHTML = `${episode.summary.substring(0, 150)}...`;
+      } else {
+        episodeSummary.innerHTML = `${episode.summary.substring(0, 150)}`;
+      }
+      episodeCardItem.appendChild(episodeSummary);
     }
-    episodeCardItem.appendChild(episodeSummary);
+
+    //Open Link
+    episodeCardItem.addEventListener("click", function () {
+      if (episode.url) {
+        window.location.assign(episode.url);
+      }
+    });
 
     episodeCard.appendChild(episodeCardItem);
   });
-
-  //Show Displaying
-  const episodeDisplaying = document.getElementById("episodeDisplaying");
-  episodeDisplaying.className = "display";
-  episodeDisplaying.innerText = `Displaying ${episodes.length}/73`;
 
   gallerySeries.appendChild(episodeCard);
 }
--- script.js
+++ script.js
@@ -1,17 +1,60 @@
 //You can edit ALL of the code here
 import { episodeCard } from "./episodes/episodeCard.js";
+import { getAllShows } from "./shows.js";
 
-const allEpisodes = getAllEpisodes();
+const errorMessage = document.getElementById("errorMessage");
+const episodeDisplaying = document.getElementById("episodeDisplaying");
+episodeDisplaying.className = "display";
+const optionListSerie = document.getElementById("optionListSerie");
 const optionListEpisode = document.getElementById("optionListEpisode");
+let allEpisodes = [];
 
-//Create Gallery
-function setup() {
-  buildGallery(allEpisodes);
+const allSeries = getAllShows().sort((a, b) => a.name.localeCompare(b.name));
+
+//Get all data from JSON - Episodes
+async function fetchEpisodeLive(SHOW_ID) {
+  await fetch(`https://api.tvmaze.com/shows/${SHOW_ID}/episodes`)
+    .then((response) => {
+      if (response.status === 200) {
+        errorMessage.style.display = "none";
+        return response.json();
+      } else {
+        throw new Error("Not Found ...");
+      }
+    })
+    .then((data) => {
+      allEpisodes = data.map((episode) => {
+        return { ...episode };
+      });
+      setup();
+    })
+    .catch((error) => (errorMessage.style.display = "block"));
 }
 
-function buildGallery(ListAllEpisodes) {
-  episodeCard(ListAllEpisodes);
+fetchEpisodeLive(1195);
 
+//Setup all functions
+function setup() {
+  buildGallery(allEpisodes);
+  searchEpisodeByWord(allEpisodes);
+  selectSerieOnList(allSeries);
+  selectEpisodeOnList(allEpisodes);
+  episodeDisplaying.innerHTML = `Displaying: ${allEpisodes.length} episodes`;
+}
+
+//Create Gallery
+async function buildGallery(ListAllEpisodes) {
+  console.log("List of All episodes", ListAllEpisodes);
+  if (ListAllEpisodes.length > 0) {
+    episodeCard(ListAllEpisodes);
+  } else {
+    searchWordInGallery.value = "";
+    episodeDisplaying.innerHTML = `Displaying: 0 episodes`;
+  }
+}
+
+//Search episode by word
+function searchEpisodeByWord(ListAllEpisodes) {
   const searchWordInGallery = document.getElementById("searchWordInGallery");
   searchWordInGallery.addEventListener("input", (event) => {
     const searchWord = event.target.value;
@@ -20,19 +63,39 @@ function buildGallery(ListAllEpisodes) {
         episode.name.toLowerCase().includes(searchWord.toLowerCase()) ||
         episode.summary.toLowerCase().includes(searchWord.toLowerCase())
     );
-
     episodeCard(filteredEpisodeByWord);
+    episodeDisplaying.innerHTML = `Displaying: ${filteredEpisodeByWord.length}/${ListAllEpisodes.length} episodes`;
   });
 }
 
-//Select Episode on List
-SelectEpisodeOnList();
+//Select serie from List
+function selectSerieOnList(listOfSeries) {
+  listOfSeries.sort((a, b) => a.name.localeCompare(b.name));
+  listOfSeries.forEach((serie) => {
+    const selectSerie = document.createElement("option");
+    selectSerie.innerHTML = `${serie.name}`;
+    selectSerie.value = serie.id;
+    optionListSerie.appendChild(selectSerie);
+  });
+}
 
-function SelectEpisodeOnList() {
-  const allEpisodes = getAllEpisodes();
+optionListSerie.addEventListener("change", FilterSelectSerie);
 
-  allEpisodes.forEach((episode) => {
+function FilterSelectSerie() {
+  let valueSerie = optionListSerie.value;
+  fetchEpisodeLive(valueSerie);
+}
+
+//Select episode from List
+function selectEpisodeOnList(ListAllEpisodes) {
+  optionListEpisode.options.length = 0;
+
+  const selectEpisode = document.createElement("option");
+  selectEpisode.innerText = "All episodes";
+  optionListEpisode.appendChild(selectEpisode);
+  ListAllEpisodes.forEach((episode) => {
     const selectEpisode = document.createElement("option");
+    selectEpisode.innerText = "All episodes";
     selectEpisode.innerText = `S${episode.season
       .toString()
       .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
@@ -41,23 +104,38 @@ function SelectEpisodeOnList() {
     optionListEpisode.appendChild(selectEpisode);
   });
   optionListEpisode.addEventListener("change", () => {
-    const nameSelectEpisode = optionListEpisode.value.slice(9);
-    let chosenEpisode = allEpisodes.filter(
-      (episode) => episode.name === nameSelectEpisode
-    );
-    if (!nameSelectEpisode) {
-      chosenEpisode = allEpisodes;
+    if (optionListEpisode.value == "All episodes") {
+      searchWordInGallery.value = "";
+      buildGallery(allEpisodes);
+      episodeDisplaying.innerHTML = `Displaying: ${allEpisodes.length} episodes`;
+    } else {
+      const nameSelectEpisode = optionListEpisode.value.slice(9);
+      const seasonSelectEpisode = Number(optionListEpisode.value.slice(1, 3));
+      const numberSelectEpisode = Number(optionListEpisode.value.slice(4, 6));
+      let chosenEpisode = ListAllEpisodes.filter(
+        (episode) =>
+          episode.name === nameSelectEpisode &&
+          episode.season === seasonSelectEpisode &&
+          episode.number === numberSelectEpisode
+      );
+      if (!nameSelectEpisode) {
+        chosenEpisode = ListAllEpisodes;
+      }
+      const searchWordInGallery = document.getElementById(
+        "searchWordInGallery"
+      );
+      searchWordInGallery.value = "";
+      episodeCard(chosenEpisode);
+      episodeDisplaying.innerHTML = `Displaying: ${chosenEpisode.length}/${ListAllEpisodes.length} episodes`;
     }
-    const searchWordInGallery = document.getElementById("searchWordInGallery");
-    searchWordInGallery.value = "";
-    episodeCard(chosenEpisode);
   });
 }
 
-//Button action
+//Show all Episodes
 document.getElementById("showAllEpisodes").onclick = function () {
   searchWordInGallery.value = "";
   buildGallery(allEpisodes);
+  episodeDisplaying.innerHTML = `Displaying: ${allEpisodes.length} episodes`;
 };
 
 window.onload = setup;
